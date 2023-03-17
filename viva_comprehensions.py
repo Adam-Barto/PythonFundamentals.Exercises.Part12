193fc2dda2a2abda58cb9c6e00ec3c901b3ed512
Adam-Barto <adam.jbarto@gmail.com>
2023-03-17 15:10:48 -0400

    gen_dict complete

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -28,17 +28,19 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    Takes a starting value, and an ending value, then makes a dictionary with the function stratagy applied to the value.
 
 
-    :param start:
-    :param stop:
-    :param strategy:
+    :param start: starting int
+    :param stop: stopping int
+    :param strategy: The function to apply to the value
     :return:
     """
-    pass
+    new_dict = dict()
+    for i in range(start, stop):
+        new_dict[i] = strategy(i)
+
+    return new_dict
 
 
 def gen_set(val_in: str) -> Set:
